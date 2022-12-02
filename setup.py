9acbbcbfea6103c8f77dfcd8526bc4250d6a28df
10tanmay100 <chakrabortytanmay744@gmail.com>
2022-12-02 18:49:00 +0530

    base code and file,folders creation completed

--- setup.py
+++ setup.py
@@ -2,24 +2,23 @@
 from setuptools import find_packages,setup
 from typing import List
 
-#defining the function which will be responsible for installing the all requirements inside txt file
-def get_install_requirements_file()->List[str]:
-          '''
-                    installing all the mentioned requirements
-          '''
-          with open("requirements.txt","r") as requirements_file:
-                    return requirements_file.readlines()
+# #defining the function which will be responsible for installing the all requirements inside txt file
+# def get_install_requirements_file()->List[str]:
+#           '''
+#                     installing all the mentioned requirements
+#           '''
+#           with open("requirements.txt","r") as requirements_file:
+#                     return requirements_file.readlines()
 
 
 
 
 setup(
           name="Insurance_Fraud",
-          version="0.0.2",
+          version="0.0.3",
           author="Tanmay Chakraborty",
           author_email="chakrabortytanmay744@gmail.com",
           packages=find_packages("insurance_fraud"),
-          install_requires=get_install_requirements_file()
 )
 
 
