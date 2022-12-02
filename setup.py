975f3e166fd38650306dfba9e626603e40cb2fa3
10tanmay100 <chakrabortytanmay744@gmail.com>
2022-12-02 18:09:49 +0530

    base code and file,folders creation completedgit add .

---
+++ setup.py
@@ -0,0 +1,33 @@
+#use to create proper package in python
+from setuptools import find_packages,setup
+from typing import List
+
+#defining the function which will be responsible for installing the all requirements inside txt file
+def get_install_requirements_file()->List[str]:
+          '''
+                    installing all the mentioned requirements
+          '''
+          with open("requirements.txt","r") as requirements_file:
+                    return requirements_file.readlines().remove("-e .")
+
+
+
+
+setup(
+          name="Insurance_Fraud",
+          version="0.0.1",
+          author="Tanmay Chakraborty",
+          author_email="chakrabortytanmay744@gmail.com",
+          packages=find_packages("insurance_fraud"),
+          install_requires=get_install_requirements_file()
+)
+
+
+
+
+
+
+
+
+
+
